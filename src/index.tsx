de3e06a5e684c85f159794005f7b0bf3d25e2a34
gdegato <rosele.tst@gmail.com>
2023-02-08 19:40:44 -0300

    React-router-dom e ESlint

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
+import './index.css';
+import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.css';
-import Cardapio from './pages/Cardapio';
+import Router from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cardapio />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
